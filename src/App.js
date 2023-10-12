09a0c80f4e1166c60a97aafe8765a613c557b8a7
Izet Delibasic <izetdelibasic55@gmail.com>
2023-10-13 01:38:55 +0200

    Nummering quotes, updating some items

--- src/App.js
+++ src/App.js
@@ -1,34 +1,33 @@
 import React from "react";
 import axios from 'axios';
 
-
 import './App.css';
 
-class App extends React.Component{
-    state = { advice: '' };
+class App extends React.Component {
+    state = { advice: '', adviceId: 177 };
 
-    componentDidMount(){
+    componentDidMount() {
         this.fetchAdvice();
     }
-    
-    fetchAdvice = () => {
-        axios.get('https://api.adviceslip.com/advice')
-          .then((response) => {
-            const { advice } = response.data.slip;
-            
-            this.setState({ advice });
-          })
-          .catch((error) => {
+
+    fetchAdvice = async () => {
+        try {
+            const response = await axios.get('https://api.adviceslip.com/advice');
+            const data = response.data.slip;
+            document.querySelector("p").innerText = "Q U O T E # " + data.id;
+            this.setState({ advice: data.advice, adviceId: data.id });
+        } catch (error) {
             console.log(error);
-          });
-      }
-    
-    render(){
-        const { advice } = this.state;
+        }
+    }
+
+    render() {
+        const { advice, adviceId } = this.state;
 
         return (
             <div className="app">
                 <div className="card">
+                    <p>Q U O T E # {adviceId}</p>
                     <h1 className="heading">{advice}</h1>
                     <button className="button" onClick={this.fetchAdvice}>
                         <span>GIVE ME ADVICE!</span>
